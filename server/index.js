d8029280cd38ce0072ece34ff51a4aab442ff31d
Артур Доценко <arturdocenko@MacBook-Air-Artur.local>
2023-12-17 00:09:47 +0200

    add api

--- server/index.js
+++ server/index.js
@@ -23,8 +23,7 @@ dbConnect();
 
 const PORT = process.env.PORT || process.env.PORT_NODE;
 
-// app.use(cors({ origin: 'https://jolybell-client.vercel.app', credentials: true }));
-app.use(cors());
+app.use(cors({ origin: 'https://jolybell-client.vercel.app', credentials: true }));
 app.use(cookieParser());
 
 app.use(session({ secret: process.env.SECRET_KEY, resave: true, saveUninitialized: true }));
